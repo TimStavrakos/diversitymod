fd811a4e9179a11e0d018423d52fe19a87c40d2f
TimStavrakos <tim.stavrakos@gmail.com>
2016-01-16 14:21:23 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,21 +1,23 @@
-Diversity Mod for The Binding of Isaac: Rebirth
+Diversity Mod for The Binding of Isaac: Afterbirth
 -----------------------------------------------
-version 0.11
+version 0.16
 
 Created by DuneAught (twitter: @duneaught)
 with much help from awerush, Hyphenated, Inschato, pears, Zamiel & #isaac on SpeedRunsLive
+Updated to Afterbirth compatibility by InvaderTim
 
-To play The Binding of Isaac: Rebirth with Diversity Mod, just run diversitymod.exe. You do not have to move any files or uninstall other mods.
+To play The Binding of Isaac: Afterbirth with Diversity Mod, just run diversitymod.exe. You do not have to move any files or uninstall other mods.
+**Ctrl-a now works in the seed selection.**
 
-Download here: https://github.com/duneaught/diversitymod/releases
+Download here: https://github.com/TimStavrakos/diversitymod/releases
 
 
 Description
 -----------
 
-All characters start with the D6 except Eden, who gets a random space-use item as usual.
+All characters, start with the D6 except Eden, who gets a random space-use item as usual, Keepr who gets Wooden Penny, and Eve, who gets Razor Blade.
 
-All characters start with 3 additional random passive items, keeping their original items and resources.
+All characters start with **the same, as of v0.14,** 3 additional random passive items, keeping their original items and resources.
 
 Mom's Knife & Epic Fetus are removed from their respective item pools. They may be assigned as random starting items, but they will not appear in game during a playthrough.
 
@@ -32,11 +34,11 @@ Room modifications are taken directly from Balls of Steel Weekly mod 1.2:
 The Diversity Mod Program
 -------------------------
 
-The Diversity Mod program generates and installs mod files when you click "Start Diversity Mod". Then it restarts The Binding of Isaac: Rebirth game.
+The Diversity Mod program generates and installs mod files when you click "Start Diversity Mod". Then it restarts The Binding of Isaac: Afterbirth game.
 
 Characters are assigned random starting items based on a user provided seed. If no seed is provided, a random seed will be generated.
 
-When the Diversity Mod program is opened, other installed Rebirth mods are moved to a temporary folder next to the Rebirth resources folder.
+When the Diversity Mod program is opened, other installed Afterbirth mods are moved to a temporary folder next to the Rebirth resources folder.
 
 The Diversity Mod program must remain open for the mod to work, because it restores previously installed mods when closed.
 
@@ -48,7 +50,7 @@ The Diversity Mod program will not work if it is inside the Rebirth resources fo
 Known Issues
 ------------
 
-* ctrl+a doesn't select all in the seed entry field, use ctrl+/ instead
+~~ * ctrl+a doesn't select all in the seed entry field, use ctrl+/ instead~~
 * The program is not currently compatible with Linux/Mac.
 * When the program closes unexpectedly, it leaves mod files installed. To fix, see: http://pastebin.com/mfreKXXT
 
@@ -82,83 +84,90 @@ Some passive items are EXCLUDED from the random starting items. Characters canno
 * 238 - Key Piece #1
 * 239 - Key Piece #2
 * 253 - Magic Scab
+* 258 - Missing No.
 * 334 - The Body
 * 339 - Safety Pin
 * 344 - Match Book
 * 346 - A Snack
+* 355 - Mom's Pearls
+* 428 - PJs
 
-### Blue Baby Exclusions
+##Removed in v0.14
+~~ ### Blue Baby Exclusions~~
 
-* 62 - Charm of the Vampire
-* 96 - Little C.H.A.D.
-* 218 - Placenta
-* 219 - Old Bandage
-* 311 - Judas' Shadow
-* 312 - Maggy's Bow
-* 332 - Lazarus' Rags
+~~ * 62 - Charm of the Vampire~~
+~~ * 96 - Little C.H.A.D.~~
+~~ * 218 - Placenta~~
+~~ * 219 - Old Bandage~~
+~~ * 311 - Judas' Shadow~~
+~~ * 312 - Maggy's Bow~~
+~~ * 332 - Lazarus' Rags~~
 
-### Azazel Exclusions
+~~ ### Azazel Exclusions~~
 
-* 3 - Spoon Bender
-* 5 - My Reflection
-* 20 - Transcendence
-* 48 - Cupids Arrow
-* 53 - Magneto
-* 60 - The Ladder
-* 68 - Technology
-* 82 - Lord of the Pit
-* 104 - The Parasite
-* 115 - Ouija Board
-* 118 - Brimstone
-* 150 - Tough Love
-* 159 - Spirit of the Night
-* 179 - Fate
-* 184 - Holy Grail
-* 185 - Dead Dove
-* 224 - Cricket's Body
-* 229 - Monstro's Lung
-* 245 - 20/20
-* 306 - Sagittarius
-* 317 - Mysterious Liquid
-* 336 - Dead Onion
+~~ * 3 - Spoon Bender~~
+~~ * 5 - My Reflection~~
+~~ * 20 - Transcendence~~
+~~ * 48 - Cupids Arrow~~
+~~ * 53 - Magneto~~
+~~ * 60 - The Ladder~~
+~~ * 68 - Technology~~
+~~ * 82 - Lord of the Pit~~
+~~ * 104 - The Parasite~~
+~~ * 115 - Ouija Board~~
+~~ * 118 - Brimstone~~
+~~ * 150 - Tough Love~~
+~~ * 159 - Spirit of the Night~~
+~~ * 179 - Fate~~
+~~ * 184 - Holy Grail~~
+~~ * 185 - Dead Dove~~
+~~ * 224 - Cricket's Body~~
+~~ * 229 - Monstro's Lung~~
+~~ * 245 - 20/20~~
+~~ * 306 - Sagittarius~~
+~~ * 317 - Mysterious Liquid~~
+~~ * 336 - Dead Onion~~
 
-### The Lost Exclusions
+~~ ### The Lost Exclusions~~
 
-* 20 - Transcendence
-* 60 - The Ladder
-* 62 - Charm of the Vampire
-* 96 - Little C.H.A.D.
-* 98 - The Relic
-* 108 - The Wafer
-* 117 - Dead Bird
-* 142 - Scapular
-* 148 - Infestation
-* 156 - Habit
-* 157 - Blood Lust
-* 161 - Ankh
-* 162 - Celtic Cross
-* 173 - Mitre
-* 178 - Holy Water
-* 180 - The Black Bean
-* 204 - Fanny Pack
-* 205 - Sharp Plug
-* 211 - Spiderbaby
-* 214 - Anemic
-* 218 - Placenta
-* 219 - Old Bandage
-* 225 - Gimpy
-* 227 - Piggy Bank
-* 262 - Missing page 2
-* 274 - Best Bud
-* 278 - Dark Bum
-* 311 - Judas' Shadow
-* 312 - Maggy's Bow
-* 327 - The Polaroid
-* 328 - The Negative
-* 332 - Lazarus' Rags
+~~ * 20 - Transcendence~~
+~~ * 60 - The Ladder~~
+~~ * 62 - Charm of the Vampire~~
+~~ * 96 - Little C.H.A.D.~~
+~~ * 98 - The Relic~~
+~~ * 108 - The Wafer~~
+~~ * 117 - Dead Bird~~
+~~ * 142 - Scapular~~
+~~ * 148 - Infestation~~
+~~ * 156 - Habit~~
+~~ * 157 - Blood Lust~~
+~~ * 161 - Ankh~~
+~~ * 162 - Celtic Cross~~
+~~ * 173 - Mitre~~
+~~ * 178 - Holy Water~~
+~~ * 180 - The Black Bean~~
+~~ * 204 - Fanny Pack~~
+~~ * 205 - Sharp Plug~~
+~~ * 211 - Spiderbaby~~
+~~ * 214 - Anemic~~
+~~ * 218 - Placenta~~
+~~ * 219 - Old Bandage~~
+~~ * 225 - Gimpy~~
+~~ * 227 - Piggy Bank~~
+~~ * 262 - Missing page 2~~
+~~ * 274 - Best Bud~~
+~~ * 278 - Dark Bum~~
+~~ * 311 - Judas' Shadow~~
+~~ * 312 - Maggy's Bow~~
+~~ * 327 - The Polaroid~~
+~~ * 328 - The Negative~~
+~~ * 332 - Lazarus' Rags~~
 
+~~ ### Lilith Exclusions(incomplete)~~
+
+~~ * 244 - Tech .5~~
 
 History
 -------
 
-For full version history, see: https://github.com/duneaught/diversitymod/releases
+For full version history, see: https://github.com/TimStavrakos/diversitymod/releases
--- diversitymod.py
+++ diversitymod.py
@@ -1,4 +1,6 @@
 import shutil, os, ConfigParser
+import xml.etree.ElementTree as ET
+import time
 from string import ascii_uppercase, digits
 from Tkinter import *
 from tkFileDialog import askopenfilename
@@ -6,6 +8,16 @@ from random import seed, randint, choice, shuffle
 from PIL import Image, ImageFont, ImageDraw, ImageTk
 from binascii import crc32
 from subprocess import call
+
+
+##
+##Constants
+##
+items_xml = ET.parse('diversitymod files/items.xml')
+
+items_info = items_xml.getroot()
+
+
 
 ##
 ## Get Steam path ( lines from http://code.activestate.com/recipes/578689-get-a-value-un-windows-registry/ )
@@ -38,6 +50,20 @@ SteamPath = regkey_value(r"HKEY_CURRENT_USER\Software\Valve\Steam", "SteamPath")
 ## end of copied stuff
 ##
 
+def delItemPool(id):
+	id = str(id)
+	if id.isdigit():
+		for xmlPool in item_pools_info:
+			for item in xmlPool:
+				if item.attrib['Id'] == id:
+					xmlPool.remove(item)
+					
+
+
+def selectall(event = None):
+	sentry.select_range(0, 'end')
+	return 'break'
+
 def newRandomSeed():
 	global seedIsRNG
 	seed()
@@ -45,6 +71,12 @@ def newRandomSeed():
 	entryseed.set(seedIsRNG)
 	
 def installDiversityMod():
+	
+	item_pools_xml = ET.parse('diversitymod files/itempools.xml')
+	players_xml = ET.parse('diversitymod files/players.xml')
+	item_pools_info = item_pools_xml.getroot()
+	players_info = players_xml.getroot()
+	
 	# trim the whitespace on the string if there is any
 	s = entryseed.get()
 	entryseed.set(s.strip().encode('ascii','replace'))
@@ -60,86 +92,102 @@ def installDiversityMod():
 	sentry.configure(bg = '#d8fbf8')
 	
 	# valid_items is the list of all passive items in the game EXCLUDING the 22 on the no-list
-	valid_items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 27, 28, 32, 46, 48, 50, 51, 52, 53, 54, 55, 57, 60, 62, 63, 64, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 79, 80, 81, 82, 87, 88, 89, 90, 91, 94, 95, 96, 98, 99, 100, 101, 103, 104, 106, 108, 109, 110, 112, 113, 114, 115, 116, 117, 118, 120, 121, 122, 125, 128, 129, 131, 132, 134, 138, 139, 140, 141, 142, 143, 144, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 159, 161, 162, 163, 165, 167, 168, 169, 170, 172, 173, 174, 178, 179, 180, 182, 183, 184, 185, 187, 188, 189, 190, 191, 193, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 227, 228, 229, 230, 231, 232, 233, 234, 236, 237, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 254, 255, 256, 257, 258, 259, 260, 261, 262, 264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 299, 300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 327, 328, 329, 330, 331, 332, 333, 335, 336, 337, 340, 341, 342, 343, 345]
+	valid_items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 27, 28, 32, 46, 48, 50, 51, 52, 53, 54, 55, 57, 60, 62, 63, 64, 67, 68, 69, 
+	70, 71, 72, 73, 74, 75, 76, 79, 80, 81, 82, 87, 88, 89, 90, 91, 94, 95, 96, 98, 99, 100, 101, 103, 104, 106, 108, 109, 110, 112, 113, 114, 115, 116, 117, 118, 
+	120, 121, 122, 125, 128, 129, 131, 132, 134, 138, 139, 140, 141, 142, 143, 144, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 159, 161, 162, 163, 165, 167, 
+	168, 169, 170, 172, 173, 174, 178, 179, 180, 182, 183, 184, 185, 187, 188, 189, 190, 191, 193, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 
+	208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 227, 228, 229, 230, 231, 232, 233, 234, 236, 237, 240, 241, 242, 243, 
+	244, 245, 246, 247, 248, 249, 250, 251, 252, 254, 255, 256, 257, 259, 260, 261, 262, 264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 
+	279, 280, 281, 299, 300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 327, 328, 329, 330, 331, 
+	332, 333, 335, 336, 337, 340, 341, 342, 343, 345, 350, 353, 354, 356, 358, 359, 360, 361, 362, 363, 364, 365, 366, 367, 368, 369, 370, 371, 372, 373, 374, 375, 
+	376, 377, 378, 379, 380, 381, 384, 385, 387, 388, 389, 390, 391, 392, 393, 394, 395, 397, 398, 399, 400, 401, 402, 403, 404, 405, 407, 408, 409, 410, 411, 412, 
+	413, 414, 415, 416, 417, 418, 420, 423, 424, 425, 426, 429, 430, 431, 432, 433, 435, 436, 438, 440]
 	
 	# random permutation of valid items list
 	itemIDs = list(valid_items)
 	shuffle(itemIDs)
-	for x in range(6, 9): # cain
-		if itemIDs[x] == 46:
-			itemIDs.append(itemIDs[x])
-			del itemIDs[x]
-	for x in range(12, 15): # blue baby
-		while itemIDs[x] in [62, 96, 218, 219, 311, 312, 332]:
-			itemIDs.append(itemIDs[x])
-			del itemIDs[x]
-	for x in range(15, 18): # eve
-		while itemIDs[x] in [122, 117]:
-			itemIDs.append(itemIDs[x])
-			del itemIDs[x]
-	for x in range(18, 21): # samson
-		if itemIDs[x] == 157:
-			itemIDs.append(itemIDs[x])
-			del itemIDs[x]
-	for x in range(21, 24): # azazel
-		while itemIDs[x] in [3, 5, 20, 48, 53, 60, 68, 82, 104, 115, 118, 150, 159, 179, 184, 185, 224, 229, 245, 306, 317, 336]:
-			itemIDs.append(itemIDs[x])
-			del itemIDs[x]
-	for x in range(24, 27): # lazarus
-		if itemIDs[x] == 332:
-			itemIDs.append(itemIDs[x])
-			del itemIDs[x]
-	for x in range(30, 33): # the lost
-		while itemIDs[x] in [20, 60, 62, 96, 98, 108, 117, 142, 148, 156, 157, 161, 162, 173, 178, 180, 204, 205, 211, 214, 218, 219, 225, 227, 262, 274, 278, 311, 312, 327, 328, 332]:
-			itemIDs.append(itemIDs[x])
-			del itemIDs[x]
-			
-	# create string
-	spaceitem = ['105,','45,', '', '34,', '36,', '', '', '', '', '', '']
-	if d6start.get():
-		spaceitem = ['105,','105,', '105,', '105,', '105,', '105,', '105,', '105,', '105,', '', '105,']
-	pxmlString = '''<players root="resources/gfx/characters/costumes/" portraitroot="resources/gfx/ui/boss/" bigportraitroot="resources/gfx/ui/stage/">
-		<player id="0" name="Isaac" skin="Character_001_Isaac.png" hp="6" bombs="1"  items="''' + spaceitem[0] + str(itemIDs[0]) + ''',''' + str(itemIDs[1]) + ''',''' + str(itemIDs[2]) + '''" portrait="PlayerPortrait_01_Isaac.png" bigportrait="PlayerPortraitBig_01_Isaac.png" skinColor="-1" />
-		<player id="1" name="Magdalene" skin="Character_002_Magdalene.png" costume="7" hp="8" items="''' + spaceitem[1] + str(itemIDs[3]) + ''',''' + str(itemIDs[4]) + ''',''' + str(itemIDs[5]) + '''" portrait="PlayerPortrait_02_Magdalene.png" bigportrait="PlayerPortraitBig_02_Magdalene.png" skinColor="-1" />
-		<player id="2" name="Cain" skin="Character_003_Cain.png" costume="8" hp="4" keys="1" items="46,''' + spaceitem[2] + str(itemIDs[6]) + ''',''' + str(itemIDs[7]) + ''',''' + str(itemIDs[8]) + '''" portrait="PlayerPortrait_03_Cain.png" bigportrait="PlayerPortraitBig_03_Cain.png" skinColor="-1" />
-		<player id="3" name="Judas" skin="Character_004_Judas.png" costume="9" hp="2" coins="3" items="''' + spaceitem[3] + str(itemIDs[9]) + ''',''' + str(itemIDs[10]) + ''',''' + str(itemIDs[11]) + '''" portrait="PlayerPortrait_04_Judas.png" bigportrait="PlayerPortraitBig_04_Judas.png" skinColor="-1" />
-		<player id="4" name="???" skin="Character_006_Bluebaby.png" hp="0" armor="6" items="''' + spaceitem[4] + str(itemIDs[12]) + ''',''' + str(itemIDs[13]) + ''',''' + str(itemIDs[14]) + '''" portrait="PlayerPortrait_06_BlueBaby.png" bigportrait="PlayerPortraitBig_06_Bluebaby.png" skinColor="2" />
-		<player id="5" name="Eve" skin="Character_005_Eve.png" costume="10" hp="4" items="122,117,''' + spaceitem[5] + str(itemIDs[15]) + ''',''' + str(itemIDs[16]) + ''',''' + str(itemIDs[17]) + '''" portrait="PlayerPortrait_05_Eve.png" bigportrait="PlayerPortraitBig_05_Eve.png" skinColor="-1" />
-		<player id="6" name="Samson" skin="Character_007_Samson.png" costume="13" hp="6" items="157,''' + spaceitem[6] + str(itemIDs[18]) + ''',''' + str(itemIDs[19]) + ''',''' + str(itemIDs[20]) + '''" portrait="PlayerPortrait_07_Samson.png" bigportrait="PlayerPortraitBig_07_Samson.png" skinColor="-1" />
-		<player id="7" name="Azazel" skin="Character_008_Azazel.png" costume="11" hp="0" black="6" card="1" items="''' + spaceitem[7] + str(itemIDs[21]) + ''',''' + str(itemIDs[22]) + ''',''' + str(itemIDs[23]) + '''" portrait="PlayerPortrait_08_Azazel.png" bigportrait="PlayerPortraitBig_08_Azazel.png" skinColor="1" />
-		<player id="8" name="Lazarus" skin="Character_009_Lazarus.png" hp="6" pill="1" items="''' + spaceitem[8] + str(itemIDs[24]) + ''',''' + str(itemIDs[25]) + ''',''' + str(itemIDs[26]) + '''" portrait="PlayerPortrait_09_Lazarus.png" bigportrait="PlayerPortraitBig_09_Lazarus.png" skinColor="-1" />
-		<player id="9" name="Eden" skin="Character_009_Eden.png" items="''' + spaceitem[9] + str(itemIDs[27]) + ''',''' + str(itemIDs[28]) + ''',''' + str(itemIDs[29]) + '''" costume="12" portrait="PlayerPortrait_09_Eden.png" bigportrait="PlayerPortraitBig_09_Eden.png" skinColor="-1">
-			<hair gfx="Character_009_EdenHair1.png" />
-			<hair gfx="Character_009_EdenHair2.png" />
-			<hair gfx="Character_009_EdenHair3.png" />
-			<hair gfx="Character_009_EdenHair4.png" />
-			<hair gfx="Character_009_EdenHair5.png" />
-			<hair gfx="Character_009_EdenHair6.png" />
-			<hair gfx="Character_009_EdenHair7.png" />
-			<hair gfx="Character_009_EdenHair8.png" />
-			<hair gfx="Character_009_EdenHair9.png" />
-			<hair gfx="Character_009_EdenHair10.png" />
-		</player>
-		<player id="10" name="The Lost" skin="Character_012_TheLost.png" hp="0" armor="1" coins="1" items="''' + spaceitem[10] + str(itemIDs[30]) + ''',''' + str(itemIDs[31]) + ''',''' + str(itemIDs[32]) + '''" portrait="PlayerPortrait_12_TheLost.png" bigportrait="PlayerPortraitBig_12_TheLost.png" skinColor="0" />
-		<player id="11" name="Lazarus II" skin="Character_010_Lazarus2.png" hp="2" items="214" portrait="PlayerPortrait_10_Lazarus2.png" bigportrait="PlayerPortraitBig_10_Lazarus2.png" skinColor="-1" />
-		<player id="12" name="Black Judas" skin="Character_013_BlackJudas.png" black="4" portrait="PlayerPortrait_BlackJudas.png" bigportrait="PlayerPortraitBig_BlackJudas.png" skinColor="1" />
-	</players>'''
 	
+	spaceitem = ['105,','45,', '', '34,', '36,', '', '', '', '', '', '284,', '357,', '349,']
+	if d6start.get():
+		spaceitem = ['105,','105,', '105,', '105,', '105,', '105,', '105,', '105,', '105,', '', '105,', '105,', '105,']
+		
+	
+	
+	#Commented out Character exclusions
+	
+	
+	#for x in range(6, 9): # cain
+	#	if itemIDs[x] == 46:
+	#		itemIDs.append(itemIDs[x])
+	#		del itemIDs[x]
+	#for x in range(12, 15): # blue baby
+	#	while itemIDs[x] in [62, 96, 218, 219, 311, 312, 332]:
+	#		itemIDs.append(itemIDs[x])
+	#		del itemIDs[x]
+	#for x in range(15, 18): # eve
+	#	while itemIDs[x] in [122, 117]:
+	#		itemIDs.append(itemIDs[x])
+	#		del itemIDs[x]
+	#for x in range(18, 21): # samson
+	#	if itemIDs[x] == 157:
+	#		itemIDs.append(itemIDs[x])
+	#		del itemIDs[x]
+	#for x in range(21, 24): # azazel
+	#	while itemIDs[x] in [3, 5, 20, 48, 53, 60, 68, 82, 104, 115, 118, 150, 159, 179, 184, 185, 224, 229, 245, 306, 317, 336]:
+	#		itemIDs.append(itemIDs[x])
+	#		del itemIDs[x]
+	#for x in range(24, 27): # lazarus
+	#	if itemIDs[x] == 332:
+	#		itemIDs.append(itemIDs[x])
+	#		del itemIDs[x]
+	#for x in range(30, 33): # the lost
+	#	while itemIDs[x] in [20, 60, 62, 96, 98, 108, 117, 142, 148, 156, 157, 161, 162, 173, 178, 180, 204, 205, 211, 214, 218, 219, 225, 227, 262, 274, 278, 311, 312, 327, 328, 332]:
+	#		itemIDs.append(itemIDs[x])
+	#		del itemIDs[x]
+	#for x in range(33,36): #lilith
+	#	while itemIDs[x] in [244]:
+	#		itemIDs.append(itemIDs[x])
+	#		del itemIDs[x]
+	#for x in range(36, 39): #keeper
+	#	while itemIDs[x] in [2]:
+	#		itemIDs.append(itemIDs[x])
+	#		del itemIDs[x]
+	
+	
+
 	# clear out the files
 	# remove all the files and folders EXCEPT packed and dmtmpfolder
 	for resourcefile in os.listdir(resourcepath):
-		if resourcefile != 'packed' and resourcefile != dmtmpfolder :
+		if resourcefile != 'packed' and resourcefile != dmtmpfolder and resourcefile != 'config.ini' :
 			if os.path.isfile(os.path.join(resourcepath, resourcefile)):
 				os.unlink(os.path.join(resourcepath, resourcefile))
 			elif os.path.isdir(os.path.join(resourcepath, resourcefile)):
 				shutil.rmtree(os.path.join(resourcepath, resourcefile))
 	
 	# write players file
-	file = open(resourcepath + "/players.xml", "w")
-	file.write(pxmlString)
-	file.close()
-        with open("diversity_seed.txt", "w") as f:
-            f.write(dmseed)
+	if os.path.isfile(resourcepath + '/players.xml'):
+		os.remove(resourcepath + '/players.xml')
+
+	shutil.copy(currentpath + '/diversitymod files/players.xml', resourcepath + '/players.xml')
+	counter = 0
+	for player in players_info:
+		if player.attrib['name'] == 'Black Judas' or player.attrib['name'] == 'Lazarus II':
+			pass
+		elif player.attrib['name'] == 'Keeper' or player.attrib['name'] == 'Eden':
+			defaultItems = str(player.get('items'))
+			player.set('items', str(itemIDs[0]) + ', ' + str(itemIDs[1]) + ', ' + str(itemIDs[2]))
+			counter += 1
+		
+		else:
+			defaultItems = str(player.get('items'))
+			player.set('items', defaultItems + ', ' + spaceitem[counter] + str(itemIDs[0]) + ', ' + str(itemIDs[1]) + ', ' + str(itemIDs[2]))
+			counter += 1
+	
+	players_xml.write(resourcepath + '/players.xml')
+	
+	file = open("diversity_seed.txt", "w")
+	file.write(dmseed)
+	
             
 	# make the graphics and install them
 	# create gfx folder structure
@@ -147,6 +195,7 @@ def installDiversityMod():
 	# open menu graphics & write the seed on them
 	characterimg = Image.open(currentpath + "/diversitymod files/charactermenu.png")
 	titleimg = Image.open(currentpath + "/diversitymod files/titlemenu.png")
+	splashimg = Image.open(currentpath + "/diversitymod files/splashes.png")
 	characterdraw = ImageDraw.Draw(characterimg)
 	titledraw = ImageDraw.Draw(titleimg)
 	smallfont = ImageFont.truetype(os.getcwd() + "/diversitymod files/comicbd.ttf", 10)
@@ -164,16 +213,19 @@ def installDiversityMod():
 	titledraw.text((435,240), "v" + str(version), (54, 47, 45), font = largefont)
 	characterimg.save(resourcepath + '/gfx/ui/main menu/charactermenu.png')
 	titleimg.save(resourcepath + '/gfx/ui/main menu/titlemenu.png')
+	splashimg.save(resourcepath + '/gfx/ui/main menu/splashes.png')
 	
-	# copy the appropriate itempools.xml if isaac's heart or soymilk is in any of the characters' starting items
-	if (276 in itemIDs[0:33]) and (330 in itemIDs[0:33]):
-		shutil.copyfile(currentpath + '/diversitymod files/itempools_nobloodrights_nolibra.xml', resourcepath + '/itempools.xml')
-	elif 276 in itemIDs[0:33]:
-		shutil.copyfile(currentpath + '/diversitymod files/itempools_nobloodrights.xml', resourcepath + '/itempools.xml')
-	elif 330 in itemIDs[0:33]:
-		shutil.copyfile(currentpath + '/diversitymod files/itempools_nolibra.xml', resourcepath + '/itempools.xml')
-	else:
-		shutil.copyfile(currentpath + '/diversitymod files/itempools.xml', resourcepath + '/itempools.xml')
+	#Copy itempools.xml to resources and remove, soy/libra from pools when present in the start, and blood rights from pools when Isaac's heart is present
+	shutil.copyfile(currentpath + '/diversitymod files/itempools.xml', resourcepath + '/itempools.xml')
+	for x in range(0,3):
+		if(itemIDs[x] == 330 or itemIDs[x] == 304):
+			delItemPool(330)
+			delItemPool(304)
+		if(itemIDs[x] == 276):
+			delItemPool(186)
+	
+	item_pools_xml.write(resourcepath + '/itempools.xml')
+	# clear out the files
 	shutil.copyfile(currentpath + '/diversitymod files/items.xml', resourcepath + '/items.xml')
 	
 	os.makedirs(resourcepath + '/rooms/')
@@ -250,7 +302,7 @@ def checkInstalled(*args):
 			sentry.configure(bg = '#f4e6e6')
 
 
-version = 0.11
+version = 0.15
 	
 # dm is the gui, entryseed is the rng seed, feedback is the message for user
 dm = Tk()
@@ -324,7 +376,8 @@ Label(dmbox, text = "Enter seed (case sensitive)", font = "font 14").grid(row = 
 sentry = Entry(dmbox, justify = CENTER, font = "font 32 bold", width = 15, textvariable = entryseed)
 sentry.configure(bg = '#f4e6e6')
 sentry.bind("<Return>", (lambda event: installDiversityMod()))
-#sentry.bind('<Control-a>', self.ctext_selectall)
+sentry.bind("<Control-a>", selectall)
+#sentry.selectall(self, "<Control-a>")
 sentry.grid(row = 1, padx = 7, columnspan = 2, pady = 5)
 # button to choose a new random seed
 dmrandimage = Image.open("diversitymod files/d100.png")
