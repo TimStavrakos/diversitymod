ac5c856ec190cdad95b76fb4fdc634bebf90921f
TimStavrakos <tim.stavrakos@gmail.com>
2016-01-16 20:41:54 -0500

    fixed room files

--- diversitymod.py
+++ diversitymod.py
@@ -93,16 +93,40 @@ def installDiversityMod():
     sentry.configure(bg = '#d8fbf8')
 
     # valid_items is the list of all passive items in the game EXCLUDING the 22 on the no-list
-    valid_items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 27, 28, 32, 46, 48, 50, 51, 52, 53, 54, 55, 57, 60, 62, 63, 64, 67, 68, 69, 
-    70, 71, 72, 73, 74, 75, 76, 79, 80, 81, 82, 87, 88, 89, 90, 91, 94, 95, 96, 98, 99, 100, 101, 103, 104, 106, 108, 109, 110, 112, 113, 114, 115, 116, 117, 118, 
-    120, 121, 122, 125, 128, 129, 131, 132, 134, 138, 139, 140, 141, 142, 143, 144, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 159, 161, 162, 163, 165, 167, 
-    168, 169, 170, 172, 173, 174, 178, 179, 180, 182, 183, 184, 185, 187, 188, 189, 190, 191, 193, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 
-    208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 227, 228, 229, 230, 231, 232, 233, 234, 236, 237, 240, 241, 242, 243, 
-    244, 245, 246, 247, 248, 249, 250, 251, 252, 254, 255, 256, 257, 259, 260, 261, 262, 264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 
-    279, 280, 281, 299, 300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 327, 328, 329, 330, 331, 
-    332, 333, 335, 336, 337, 340, 341, 342, 343, 345, 350, 353, 354, 356, 358, 359, 360, 361, 362, 363, 364, 365, 366, 367, 368, 369, 370, 371, 372, 373, 374, 375, 
-    376, 377, 378, 379, 380, 381, 384, 385, 387, 388, 389, 390, 391, 392, 393, 394, 395, 397, 398, 399, 400, 401, 402, 403, 404, 405, 407, 408, 409, 410, 411, 412, 
-    413, 414, 415, 416, 417, 418, 420, 423, 424, 425, 426, 429, 430, 431, 432, 433, 435, 436, 438, 440]
+    valid_items =  [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+        11, 12, 13, 14, 17, 18, 19, 20, 21, 27,
+        28, 32, 46, 48, 50, 51, 52, 53, 54, 55,
+        57, 60, 62, 63, 64, 67, 68, 69, 70, 71,
+        72, 73, 74, 75, 76, 79, 80, 81, 82, 87,
+        88, 89, 90, 91, 94, 95, 96, 98, 99, 100,
+        101, 103, 104, 106, 108, 109, 110, 112, 113, 114,
+        115, 116, 117, 118, 120, 121, 122, 125, 128, 129,
+        131, 132, 134, 138, 139, 140, 141, 142, 143, 144,
+        148, 149, 150, 151, 152, 153, 154, 155, 156, 157,
+        159, 161, 162, 163, 165, 167, 168, 169, 170, 172,
+        173, 174, 178, 179, 180, 182, 183, 184, 185, 187,
+        188, 189, 190, 191, 193, 195, 196, 197, 198, 199,
+        200, 201, 202, 203, 204, 205, 206, 207, 208, 209,
+        210, 211, 212, 213, 214, 215, 216, 217, 218, 219,
+        220, 221, 222, 223, 224, 225, 227, 228, 229, 230,
+        231, 232, 233, 234, 236, 237, 240, 241, 242, 243,
+        244, 245, 246, 247, 248, 249, 250, 251, 252, 254,
+        255, 256, 257, 259, 260, 261, 262, 264, 265, 266,
+        267, 268, 269, 270, 271, 272, 273, 274, 275, 276,
+        277, 278, 279, 280, 281, 299, 300, 301, 302, 303,
+        304, 305, 306, 307, 308, 309, 310, 311, 312, 313,
+        314, 315, 316, 317, 318, 319, 320, 321, 322, 327,
+        328, 329, 330, 331, 332, 333, 335, 336, 337, 340,
+        341, 342, 343, 345, 350, 353, 354, 356, 358, 359,
+        360, 361, 362, 363, 364, 365, 366, 367, 368, 369,
+        370, 371, 372, 373, 374, 375, 376, 377, 378, 379,
+        380, 381, 384, 385, 387, 388, 389, 390, 391, 392,
+        393, 394, 395, 397, 398, 399, 400, 401, 402, 403,
+        404, 405, 407, 408, 409, 410, 411, 412, 413, 414,
+        415, 416, 417, 418, 420, 423, 424, 425, 426, 429,
+        430, 431, 432, 433, 435, 436, 438, 440
+    ]
 
     # random permutation of valid items list
     itemIDs = list(valid_items)
@@ -179,25 +203,33 @@ def installDiversityMod():
     #Copy itempools.xml to resources and remove, soy/libra from pools when present in the start, and blood rights from pools when Isaac's heart is present
     shutil.copyfile(currentpath + '/resources/itempools.xml', resourcepath + '/itempools.xml')
     for x in range(0,3):
-        if(itemIDs[x] == 330 or itemIDs[x] == 304):
+        if itemIDs[x] == 330 or itemIDs[x] == 304:
             delItemPool(330)
             delItemPool(304)
-        if(itemIDs[x] == 276):
+        if itemIDs[x] == 276:
             delItemPool(186)
+        if itemIDs[x] == 118:
+            delItemPool(38)
+        if itemIDs[x] == 229 or itemIDs[x] == 69:
+            delItemPool(229)
+            delItemPool(69)
+        if itemIDs[x] == 149 or itemIDs[x] == 168:
+            delItemPool(149)
+            delItemPool(168)
+        if itemIDs[x] == 152:
+            delItemPool(149)
+        if itemIDs[x] == 229:
+            delItemPool(149)
 
     item_pools_xml.write(resourcepath + '/itempools.xml')
     # clear out the files
     shutil.copyfile(currentpath + '/resources/items.xml', resourcepath + '/items.xml')
 
     os.makedirs(resourcepath + '/rooms/')
-    shutil.copyfile(currentpath + '/resources/rooms/00.special rooms.stb', resourcepath + '/rooms/00.special rooms.stb')
-    shutil.copyfile(currentpath + '/resources/rooms/04.catacombs.stb', resourcepath + '/rooms/04.catacombs.stb')
-    shutil.copyfile(currentpath + '/resources/rooms/05.depths.stb', resourcepath + '/rooms/05.depths.stb')
-    shutil.copyfile(currentpath + '/resources/rooms/06.necropolis.stb', resourcepath + '/rooms/06.necropolis.stb')
-    shutil.copyfile(currentpath + '/resources/rooms/07.womb.stb', resourcepath + '/rooms/07.womb.stb')
-    shutil.copyfile(currentpath + '/resources/rooms/08.utero.stb', resourcepath + '/rooms/08.utero.stb')
-    shutil.copyfile(currentpath + '/resources/rooms/09.sheol.stb', resourcepath + '/rooms/09.sheol.stb')
-    shutil.copyfile(currentpath + '/resources/rooms/12.chest.stb', resourcepath + '/rooms/12.chest.stb')
+    for files in os.listdir(currentpath + '/resources/rooms'):
+            shutil.copyfile(currentpath + '/resources/rooms/' + str(files), resourcepath + '/rooms/' + str(files))
+
+
 
     # update gui stuffs
     dm.update_idletasks()
@@ -265,7 +297,7 @@ def checkInstalled(*args):
             sentry.configure(bg = '#f4e6e6')
 
 
-version = 1.00
+version = 1.0
 
 # dm is the gui, entryseed is the rng seed, feedback is the message for user
 dm = Tk()
@@ -361,4 +393,4 @@ Message(dm, justify = CENTER, text = "Rebirth returns to normal when this progra
 # uninstall mod files when the window is closed
 dm.protocol("WM_DELETE_WINDOW", closeDiversityMod)
 
-mainloop( )
+mainloop()
